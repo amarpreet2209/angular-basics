4993fbebc709b55f4daaed9cbe20c669a5f55be1
Amarpreet Singh <amarpreet2209@gmail.com>
2017-06-20 19:26:26 +0530

    Basics completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,18 @@ import { Component } from '@angular/core';
   `]
 })
 export class AppComponent {
+  showSecret = false;
+  counter: number=0;
+  log = [];
+  constructor() {
+
+  }
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+
+  }
 
 }
